9f2f0a5dca364d5033374c0ee9b717c8e01bdb44
ap82 <ap82@IN5CG4525JV9.CSCMWS.CSCMWS.COM>
2019-02-20 15:11:03 +0530

    new commit

---
+++ src/main/java/com/ara/maven/arafath/App.java
@@ -0,0 +1,13 @@
+package com.ara.maven.arafath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
